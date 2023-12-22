86f11ec4fe0acc576d973667637ff67a03b2508f
criticalsession <criticalsession@protonmail.ch>
2023-12-22 06:03:18 +0100

    Get all images in thread

--- scraper.go
+++ scraper.go
@@ -10,8 +10,12 @@ func main() {
 	fmt.Println("* 4scraper *")
 
 	c := colly.NewCollector(
-		colly.AllowedDomains("boards.4chan.org"),
+		colly.AllowedDomains("boards.4chan.org", "i.4cdn.org"),
 	)
+
+	c.OnHTML(".fileText > a", func(h *colly.HTMLElement) {
+		fmt.Printf("--> Text: %v, URL: %v\n", h.Text, h.Request.AbsoluteURL(h.Attr("href")))
+	})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("-> Visiting", r.URL.String())
